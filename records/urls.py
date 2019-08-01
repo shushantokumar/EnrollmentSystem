72aa8e1411ea64f9a4217fdfb4d1c02591a321d3
shushanto <kvir420@gmail.com>
2019-08-01 23:04:30 +0600

    INITIALIZED

--- records/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name = 'records'),
-    path('<int:studentinfo_id>', views.studentinfo, name = 'studentinfo'),
-    path('search', views.search, name = 'search')
-]
