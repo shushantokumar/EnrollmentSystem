e8c0a27ae23dd8ce2f34fc48dabdcbcc50625b21
shushanto <kvir420@gmail.com>
2019-08-27 06:51:48 +0600

    initialized

--- enrollmentsystem/settings.py
+++ enrollmentsystem/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = 'enrollmentsystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'enrollmentsystem',
-        'USER': 'postgres',
-        'PASSWORD': '123',
-        'HOST': 'localhost'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
